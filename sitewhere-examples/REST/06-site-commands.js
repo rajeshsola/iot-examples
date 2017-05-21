2da08420b465562f31db42e4717635d1ebc8490d
rajeshsola <rajeshsola@gmail.com>
2017-05-21 12:36:20 +0530

    REST examples

---
+++ sitewhere-examples/REST/06-site-commands.js
@@ -0,0 +1,17 @@
+const request=require('request'),
+      url=require('url');
+var auth = new Buffer('admin:password').toString('base64');
+var siteToken = 'bb105f8d-3150-41f5-b9d1-db04965668d3';	//construction site
+var urlopts={
+	url:"http://127.0.0.1:8080/sitewhere/api/sites/"+siteToken+"/invocations",
+	headers : {
+		'Authorization' : 'Basic ' + auth,
+		'X-Sitewhere-Tenant': 'sitewhere1234567890'
+	}
+};
+request.get(urlopts,function(error,response,body) {
+  if(error)
+	  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', JSON.parse(body,"",4));
+});
